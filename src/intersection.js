d421980712f2e2c0ba7d675539d045149de7bd6b
Maryna <marinakukusua@gmail.com>
2026-02-22 20:42:44 +0200

    Add HW intersection

---
+++ src/intersection.js
@@ -0,0 +1,22 @@
+// function createArr () {
+//   const arr = [];
+//   for (let i = 0; i < 30; i++) {
+//     arr.push(Math.floor(Math.random() * 101));
+//   }
+//   return arr;
+// }
+
+const arr1 = Array.from({ length: 30 }, () => Math.floor(Math.random() * 100));
+
+const arr2 = Array.from({ length: 30 }, () => Math.floor(Math.random() * 100));
+
+function getIntersection (arr1, arr2) {
+  const set1 = new Set(arr1);
+  const set2 = new Set(arr2);
+  const arr = set1.intersection(set2);
+  return arr;
+}
+
+console.log(arr1);
+console.log(arr2);
+console.log(getIntersection(arr1, arr2));
